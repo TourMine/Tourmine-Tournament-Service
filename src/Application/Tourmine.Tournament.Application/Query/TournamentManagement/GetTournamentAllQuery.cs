85e8d183a51e898942d50331cb514ed5479213c9
ClodsNegreiros <clodsn@gmail.com>
2025-01-30 22:35:51 -0300

    feat: implementado endpoint getAll de Tournament

---
+++ src/Application/Tourmine.Tournament.Application/Query/TournamentManagement/GetTournamentAllQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Tourmine.Tournament.Application.Requests.TournamentManagement;
+
+namespace Tourmine.Tournament.Application.Command.TournamentManagement
+{
+    public class GetTournamentAllQuery : IRequest<List<Domain.Entities.TournamentManagement.Tournament>>
+    {
+        public int Start { get; set; }
+        public int Limit { get; set; }
+        public GetTournamentAllRequest Request {  get; set; }
+
+        public GetTournamentAllQuery(
+            int start,
+            int limit,
+            GetTournamentAllRequest request)
+        {
+            Start = start;
+            Limit = limit;
+            Request = request;
+        }
+    }
+}
