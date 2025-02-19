74a062feb41ad50237ed6fa87ebf0ea2d0f68dd6
ClodsNegreiros <clodsn@gmail.com>
2025-02-19 10:21:12 -0300

    feat: módulo de torneio dockerizado

--- src/WebAPI/Tourmine.Tournament.API/Program.cs
+++ src/WebAPI/Tourmine.Tournament.API/Program.cs
@@ -3,12 +3,21 @@ using Tourmine.Tournament.Application.Interface.TournamentManagement;
 using Tourmine.Tournament.Application.Interfaces.SubscriptionManagement;
 using Tourmine.Tournament.Application.UseCases.SubscriptionManagement;
 using Tourmine.Tournament.Application.UseCases.TournamentManagement;
+using Tourmine.Tournament.Domain.Entities.TournamentManagement;
+using Tourmine.Tournament.Domain.Enums;
 using Tourmine.Tournament.Domain.Interfaces.Repositories;
 using Tourmine.Tournament.Infrastructure;
 using Tourmine.Tournament.Infrastructure.Context;
 using Tourmine.Tournament.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona o appsettings.Docker.json quando rodando no container
+var environment = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER");
+if (!string.IsNullOrEmpty(environment))
+{
+    builder.Configuration.AddJsonFile("appsettings.Docker.json", optional: true);
+}
 
 // Adiciona os serviços necessários para Swagger
 builder.Services.AddControllers(); // Adiciona o controller
@@ -48,7 +57,10 @@ builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString(Settings.ConnectionString)));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString(Settings.ConnectionString)));
 
 var app = builder.Build();
 
@@ -61,9 +73,52 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
-app.UseCors(corsPolicy); 
 
-app.MapControllers(); 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+
+    if (!context.Tournaments.Any())
+    {
+        context.Tournaments.AddRange(
+           new Tournament
+           {
+               UserId = Guid.NewGuid(),
+               Name = "Tournament 1",
+               Game = "Game 1",
+               Plataform = EPlataforms.PC,
+               MaxTeams = 16,
+               TeamsType = EParticipantsType.DUO,
+               StartDate = DateTime.Now.AddMonths(1),
+               EndDate = DateTime.Now.AddMonths(1).AddDays(7),
+               Prize = "1000 USD",
+               SubscriptionType = ESubscriptionType.FREE,
+               Status = ETournamentStatus.Open,
+               Description = "This is the first tournament for Game 1"
+           },
+           new Tournament
+           {
+               UserId = Guid.NewGuid(),
+               Name = "Tournament 2",
+               Game = "Game 2",
+               Plataform = EPlataforms.PC,
+               MaxTeams = 16,
+               TeamsType = EParticipantsType.DUO,
+               StartDate = DateTime.Now.AddMonths(1),
+               EndDate = DateTime.Now.AddMonths(1).AddDays(7),
+               Prize = "1000 USD",
+               SubscriptionType = ESubscriptionType.FREE,
+               Status = ETournamentStatus.Open,
+               Description = "This is the first tournament for Game 1"
+           }
+        );
+    }
+}
+
+app.UseCors(corsPolicy);
+
+app.MapControllers();
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
