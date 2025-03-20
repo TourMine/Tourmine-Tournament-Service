a5039d59f85c51dca039f091433862fe6188b25c
ClodsNegreiros <clodsn@gmail.com>
2025-03-20 10:37:09 -0300

    feat: implementado cachingService para uso do redis

---
+++ src/Infrastructure/Tourmine.Tournament.Infrastructure/Persistence/Caching/CachingService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Tourmine.Tournament.Domain.Interfaces.Caching;
+
+namespace Tourmine.Tournament.Infrastructure.Persistence.Caching
+{
+    public class CachingService : ICachingService
+    {
+        private readonly IDistributedCache _cache;
+        private readonly DistributedCacheEntryOptions _options;
+
+        public CachingService(IDistributedCache cache)
+        {
+            _cache = cache;
+            _options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                SlidingExpiration = TimeSpan.FromSeconds(120)
+            };
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            return await _cache.GetStringAsync(key);
+        }
+
+        public async Task SetAsync(string key, string value)
+        {
+            await _cache.SetStringAsync(key, value, _options);
+        }
+    }
+}
